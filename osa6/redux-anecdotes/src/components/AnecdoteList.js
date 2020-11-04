1a5cb45abe15d651107ec6f8b3f1c89ca4185e8a
Tuomo Ojaluoma <ojaluoma.tuomo@gmail.com>
2020-11-04 22:13:06 +0200

    ex till 6.14

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import anecdoteService from '../services/anecdotes'
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
 import Filter from './Filter'
 
@@ -10,11 +11,24 @@ const AnecdoteList = () => {
     anecdote.content.toLowerCase().includes(filter.toLowerCase())))
   const dispatch = useDispatch()
 
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
+  const vote = async (id) => {
     const anecdote = anecdotes.find(a => a.id === id)
+    const updatedAnecdote = {...anecdote, votes: anecdote.votes + 1}
+    await anecdoteService.update(id, updatedAnecdote)
+    dispatch(voteAnecdote(id))
     showNotification(`You voted '${anecdote.content}'`)
   }
+
+  /*
+  const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
+    showNotification(`Added '${content}'`)
+  }
+  */
 
   const showNotification = (notification) => {
     dispatch(setNotification(notification))
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,28 +1,11 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+// import anecdoteService from '../services/anecdotes'
 
 const sortByVotes = (list) => {
   list.sort((a, b) => {return b.votes - a.votes})
   return list
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
@@ -30,6 +13,8 @@ const anecdoteReducer = (state = initialState, action) => {
   switch(action.type){
     case 'NEW_ANECDOTE':
       return sortByVotes([...state, action.data])
+    case 'INIT_ANECDOTES':
+      return sortByVotes(action.data)
     case 'VOTE_ANECDOTE':
       const id = action.data.id
       const anecdoteToVote = state.find(a => a.id === id)
@@ -44,17 +29,47 @@ const anecdoteReducer = (state = initialState, action) => {
   }
 }
 
-export const createAnecdote = (anecdote) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    data: asObject(anecdote)
+/*
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote
+    })
   }
 }
+*/
+export const createAnecdote = (data) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data
+  }
+}
+
 
 export const voteAnecdote = (id) => {
   return {
     type: 'VOTE_ANECDOTE',
     data: { id }
+  }
+}
+
+/*
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
+  }
+}
+*/
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes,
   }
 }
 
