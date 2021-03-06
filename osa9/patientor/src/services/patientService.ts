c16f49004f79eacfb01f8713860748a999f0c82b
Tuomo Ojaluoma <ojaluoma.tuomo@gmail.com>
2021-03-06 18:02:34 +0200

    9.13

--- osa9/patientor/src/services/patientService.ts
+++ osa9/patientor/src/services/patientService.ts
@@ -1,13 +1,19 @@
 import patientData from '../../data/patients.json';
-import { PatientEntry } from '../types';
+import { PatientEntry, NonSensitivePatientEntry, NewPatientEntry } from '../types';
+import toNewPatientEntry from '../utils';
+import {v1 as uuid} from 'uuid';
 
-const patients: Array<PatientEntry> = patientData;
+const patients: Array<PatientEntry> = patientData.map(obj => {
+    const object = toNewPatientEntry(obj) as PatientEntry;
+    object.id = obj.id;
+    return object;
+});
 
 const getEntries = (): Array<PatientEntry> => {
     return patients;
 };
 
-const getNonSensitiveEntries = (): Omit<PatientEntry, 'ssn'>[] => {
+const getNonSensitiveEntries = (): Array<NonSensitivePatientEntry> => {
     return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
         id,
         name,
@@ -17,8 +23,14 @@ const getNonSensitiveEntries = (): Omit<PatientEntry, 'ssn'>[] => {
     }));
 };
 
-const addEntry = () => {
-    return null;
+const addEntry = ( entry: NewPatientEntry ): PatientEntry => {
+    const id = uuid();
+    const newPatientEntry = {
+        id: id,
+        ...entry
+    };
+    patients.push(newPatientEntry);
+    return newPatientEntry;
 };
 
 export default {
--- osa9/patientor/src/types.ts
+++ osa9/patientor/src/types.ts
@@ -1,14 +1,23 @@
+export enum Gender {
+    Male = 'male',
+    Female = 'female'
+}
+
 export interface DiagnoseEntry {
     code: string;
     name: string;
     latin?: string;
 }
 
+export type NewPatientEntry = Omit<PatientEntry, 'id'>;
+
+export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn'>;
+
 export interface PatientEntry {
     id: string;
     name: string;
     dateOfBirth: string;
     ssn: string;
-    gender: string;
+    gender: Gender;
     occupation: string;
 }
